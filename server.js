c989ddd2edd6d4c06641e041f4fe736436be41f9
filippozuppini <zuppini.filippo@gmail.com>
2026-05-28 20:34:36 +0200

    add messages for main request

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = 3001;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
 
 app.listen(port, () => {
   console.log(`Example app listening on port http://localhost:${port}`);
+});
+
+
+app.get('/', (req, res) => {
+  console.log('Messaggio inviato');
+
+  res.send('Server del mio blog');
 });
